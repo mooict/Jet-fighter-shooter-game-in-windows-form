edf0b83eeec6e777a313dc7ccd3897b86298ced2
Anhar Ali <67914277+mooict@users.noreply.github.com>
2021-01-03 14:00:27 +0000

    Add files via upload

---
+++ Fighter Jet Shooting Game MOO ICT/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Fighter_Jet_Shooting_Game_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.enemyOne = new System.Windows.Forms.PictureBox();
+            this.enemyTwo = new System.Windows.Forms.PictureBox();
+            this.enemyThree = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enemyOne
+            // 
+            this.enemyOne.Image = global::Fighter_Jet_Shooting_Game_MOO_ICT.Properties.Resources.enemy;
+            this.enemyOne.Location = new System.Drawing.Point(26, 62);
+            this.enemyOne.Name = "enemyOne";
+            this.enemyOne.Size = new System.Drawing.Size(100, 85);
+            this.enemyOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyOne.TabIndex = 0;
+            this.enemyOne.TabStop = false;
+            // 
+            // enemyTwo
+            // 
+            this.enemyTwo.Image = global::Fighter_Jet_Shooting_Game_MOO_ICT.Properties.Resources.enemy;
+            this.enemyTwo.Location = new System.Drawing.Point(334, 62);
+            this.enemyTwo.Name = "enemyTwo";
+            this.enemyTwo.Size = new System.Drawing.Size(100, 85);
+            this.enemyTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyTwo.TabIndex = 0;
+            this.enemyTwo.TabStop = false;
+            // 
+            // enemyThree
+            // 
+            this.enemyThree.Image = global::Fighter_Jet_Shooting_Game_MOO_ICT.Properties.Resources.enemy;
+            this.enemyThree.Location = new System.Drawing.Point(664, 62);
+            this.enemyThree.Name = "enemyThree";
+            this.enemyThree.Size = new System.Drawing.Size(100, 85);
+            this.enemyThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyThree.TabIndex = 0;
+            this.enemyThree.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Image = global::Fighter_Jet_Shooting_Game_MOO_ICT.Properties.Resources.bullet;
+            this.bullet.Location = new System.Drawing.Point(473, 304);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(7, 27);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bullet.TabIndex = 0;
+            this.bullet.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Fighter_Jet_Shooting_Game_MOO_ICT.Properties.Resources.player;
+            this.player.Location = new System.Drawing.Point(334, 564);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(110, 98);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(0, 277);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(798, 150);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 674);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.enemyThree);
+            this.Controls.Add(this.enemyTwo);
+            this.Controls.Add(this.enemyOne);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Form1";
+            this.Text = "Fighet Jet Shooting Game MOOI CT";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox enemyOne;
+        private System.Windows.Forms.PictureBox enemyTwo;
+        private System.Windows.Forms.PictureBox enemyThree;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Fighter Jet Shooting Game MOO ICT/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fighter_Jet_Shooting_Game_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight, shooting, isGameOver;
+        int score;
+        int playerSpeed = 12;
+        int enemySpeed;
+        int bulletSpeed;
+        Random rnd = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetGame();
+        }
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+
+            txtScore.Text = score.ToString();
+
+
+            enemyOne.Top += enemySpeed;
+            enemyTwo.Top += enemySpeed;
+            enemyThree.Top += enemySpeed;
+
+
+            if(enemyOne.Top > 710 || enemyTwo.Top > 710 || enemyThree.Top > 710)
+            {
+                gameOver();
+            }
+
+
+
+            // player movement logic starts
+
+            if(goLeft == true && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+            if(goRight == true && player.Left < 688)
+            {
+                player.Left += playerSpeed;
+            }
+            // player movement logic ends
+
+            if(shooting == true)
+            {
+                bulletSpeed = 20;
+                bullet.Top -= bulletSpeed;
+            }
+            else
+            {
+                bullet.Left = -300;
+                bulletSpeed = 0;
+            }
+
+            if(bullet.Top < -30)
+            {
+                shooting = false;
+            }
+
+            if(bullet.Bounds.IntersectsWith(enemyOne.Bounds))
+            {
+                score += 1;
+                enemyOne.Top = -450;
+                enemyOne.Left = rnd.Next(20, 600);
+                shooting = false;
+            }
+            if (bullet.Bounds.IntersectsWith(enemyTwo.Bounds))
+            {
+                score += 1;
+                enemyTwo.Top = -650;
+                enemyTwo.Left = rnd.Next(20, 600);
+                shooting = false;
+            }
+            if (bullet.Bounds.IntersectsWith(enemyThree.Bounds))
+            {
+                score += 1;
+                enemyThree.Top = -750;
+                enemyThree.Left = rnd.Next(20, 600);
+                shooting = false;
+            }
+
+            if(score == 5)
+            {
+                enemySpeed = 10;
+            }
+            if(score == 10)
+            {
+                enemySpeed = 15;
+            }
+
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if(e.KeyCode == Keys.Space && shooting == false)
+            {
+                shooting = true;
+
+                bullet.Top = player.Top - 30;
+                bullet.Left = player.Left + (player.Width /2);
+
+            }
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                resetGame();
+            }
+        }
+
+        private void resetGame()
+        {
+            gameTimer.Start();
+            enemySpeed = 6;
+
+
+            enemyOne.Left = rnd.Next(20, 600);
+            enemyTwo.Left = rnd.Next(20, 600);
+            enemyThree.Left = rnd.Next(20, 600);
+
+            enemyOne.Top = rnd.Next(0, 200) * -1;
+            enemyTwo.Top = rnd.Next(0, 500) * -1;
+            enemyThree.Top = rnd.Next(0, 900) *-1;
+
+            score = 0;
+            bulletSpeed = 0;
+            bullet.Left = -300;
+            shooting = false;
+
+
+            txtScore.Text = score.ToString();
+
+        }
+
+        private void gameOver()
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+            txtScore.Text += Environment.NewLine + "Game Over!!" + Environment.NewLine + "Press Enter to try again.";
+
+        }
+    }
+}
